aa00b1d4a0c7a78c945b1a4d45503322e484fa4a
mw <mwwpublic@gmail.com>
2016-07-24 16:57:03 -0400

    add skeleton dotfiles

--- vagrant-bootstrap.sh
+++ vagrant-bootstrap.sh
@@ -6,4 +6,14 @@ apt-get install git-core
 git clone https://github.com/MWGitHub/dotfiles.git
 
 # Copy all dotfiles into home
-find dotfiles/home -exec ln -s . {} \;
+function link_home() {
+  for file in dotfiles/home/?*; do
+    if [[ -d $file ]]; then
+      mkdir -p $2/`basename $file`
+      link_home $file $2/`basename $file`
+    else
+      ln -s -f $file $2/`basename $file`
+    fi
+  done
+}
+link_home
