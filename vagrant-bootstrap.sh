9fbed589642ea8e3b4477d629d876f6730774738
mw <mwwpublic@gmail.com>
2016-07-24 17:01:53 -0400

    add config linking

--- vagrant-bootstrap.sh
+++ vagrant-bootstrap.sh
@@ -5,15 +5,19 @@ apt-get update
 apt-get install git-core
 git clone https://github.com/MWGitHub/dotfiles.git
 
+# From https://github.com/webcoyote/dotfiles configure.sh
 # Copy all dotfiles into home
 function link_home() {
-  for file in dotfiles/home/?*; do
+  for file in $1/?*; do
     if [[ -d $file ]]; then
       mkdir -p $2/`basename $file`
       link_home $file $2/`basename $file`
     else
+      echo linking $file '=>' $2/`basename $file`
       ln -s -f $file $2/`basename $file`
     fi
   done
 }
-link_home
+shopt -s dotglob
+link_home dotfiles/home $HOME
+shopt -u dotglob
