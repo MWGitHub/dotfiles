582c4ff560cae452baa6982b89452c0753510934
mw <mwwpublic@gmail.com>
2016-07-24 16:27:41 -0400

    add dotfile linking and script linking

--- vagrant-bootstrap.sh
+++ vagrant-bootstrap.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+cd ~
 apt-get update
+apt-get install git-core
+git clone https://github.com/MWGitHub/dotfiles.git
+
+# Copy all dotfiles into home
+find dotfiles/home -exec ln -s . {} \;
