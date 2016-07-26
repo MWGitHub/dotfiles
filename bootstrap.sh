010ffb15441483043c316962bc9b432b4d59e4af
Michael Wang <mwwpublic@gmail.com>
2016-07-25 20:16:13 -0400

    move bootstrap out of vagrant-bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+cd ~
+sudo apt-get -y update
+
+# Run install scripts
+chmod +x -R ./dotfiles/home/bin
+./dotfiles/home/bin/install-zsh.sh
+
+# From https://github.com/webcoyote/dotfiles configure.sh
+# Copy all dotfiles into home
+function link_home() {
+  for file in $1/?*; do
+    if [[ -d $file ]]; then
+      mkdir -p $2/`basename $file`
+      link_home $file $2/`basename $file`
+    else
+      echo linking $file '=>' $2/`basename $file`
+      ln -s -f $file $2/`basename $file`
+    fi
+  done
+}
+shopt -s dotglob
+link_home dotfiles/home $HOME
+shopt -u dotglob
--- vagrant-bootstrap.sh
+++ vagrant-bootstrap.sh
@@ -7,23 +7,6 @@ sudo shutdown -r now
 sudo apt-get -y install git-core
 git clone https://github.com/MWGitHub/dotfiles.git
 
-# Run install scripts
-chmod +x -R ./dotfiles/home/bin
-./dotfiles/home/bin/install-zsh.sh
-
-# From https://github.com/webcoyote/dotfiles configure.sh
-# Copy all dotfiles into home
-function link_home() {
-  for file in $1/?*; do
-    if [[ -d $file ]]; then
-      mkdir -p $2/`basename $file`
-      link_home $file $2/`basename $file`
-    else
-      echo linking $file '=>' $2/`basename $file`
-      ln -s -f $file $2/`basename $file`
-    fi
-  done
-}
-shopt -s dotglob
-link_home dotfiles/home $HOME
-shopt -u dotglob
+# Run bootstrap
+chmod +x ./dotfiles/bootstrap.sh
+./dotfiles/bootstrap.sh
