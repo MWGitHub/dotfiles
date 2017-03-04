52e5d1ed47120833473c5595fced2f674f7ea5fe
Michael Wang <mwwpublic@gmail.com>
2017-03-04 04:36:44 -0500

    check for file existance and add vim configs

--- bin/mac/install-tmux.sh
+++ bin/mac/install-tmux.sh
@@ -3,7 +3,14 @@
 brew update
 brew install tmux
 
-rm -rf ~/.tmux/plugins/tpm
+# clean up previous files
+if [ -e "$HOME/.tmux.conf" ]; then
+  rm ~/.tmux.conf
+fi
+if [ -d "$HOME/.tmux/plugins/tpm" ]; then
+  rm -rf ~/.tmux/plugins/tpm
+fi
+
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../../"
---
+++ bin/mac/install-vim.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+brew update
+brew install vim --override-system-vim
+
+curl https://j.mp/spf13-vim3 -L -o - | sh
+
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../../"
+
+# clean up previous files
+if [ -e "$HOME/.vimrc.local" ]; then
+  rm ~/.vimrc.local
+fi
+if [ -e "$HOME/.vimrc.before.local" ]; then
+  rm ~/.vimrc.before.local
+fi
+if [ -e "$HOME/.vimrc.bundles.local" ]; then
+  rm ~/.vimrc.bundles.local
+fi
+
+ln -s "$ROOT/configs/.vimrc.local" ~/.vimrc.local
+ln -s "$ROOT/configs/.vimrc.before.local" ~/.vimrc.before.local
+ln -s "$ROOT/configs/.vimrc.bundles.local" ~/.vimrc.bundles.local
