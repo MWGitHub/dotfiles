3fbc8185e537324e55279a5a0133ae75d32df622
Michael Wang <mwwpublic@gmail.com>
2018-05-13 18:18:39 -0400

    update nvm install

--- bootstrap/wsl/bootstrap.sh
+++ bootstrap/wsl/bootstrap.sh
@@ -51,7 +51,7 @@ pip3 install pipenv
 
 # Install node
 mkdir -p "$HOME/.nvm"
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash | PROFILE=/dev/null
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash | PROFILE="$HOME/.bashrc.common" NVM_DIR="$HOME/.nvm"
 
 # Reset dotfiles origins
 cd "$HOME/projects/dotfiles"
