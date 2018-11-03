db11113d362f028b2e0e83a8549ff76b52d47916
Michael Wang <mwwpublic@gmail.com>
2018-11-03 19:14:01 -0400

    chore: update terraform

--- bootstrap/wsl/bootstrap-16.04.sh
+++ bootstrap/wsl/bootstrap-16.04.sh
@@ -305,7 +305,7 @@ install_tools() {
 
   # Install terraform
   if [ ! -f "$HOME/tools/terraform" ]; then
-    wget https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
+    wget https://releases.hashicorp.com/terraform/0.11.10/terraform_0.11.10_linux_amd64.zip
     unzip terraform*.zip
     rm terraform*.zip
   fi
